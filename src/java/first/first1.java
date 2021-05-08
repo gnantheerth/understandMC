448f50986092bb2c00032453532b251da5300464
gnantheerth <gnantheerthofficial@gmail.com>
2021-05-08 22:08:10 +0530

    addtion of two numbers

---
+++ src/java/first/first1.java
@@ -0,0 +1,13 @@
+package java.first;
+
+public class first1 {
+    public static void main(String[] args) {
+        add(2,3);
+
+    }
+
+    public static void add(int a, int b) {
+        System.out.println(a+b);
+
+    }
+}
